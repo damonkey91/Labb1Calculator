a2b4ea2ad39a4ab797e8c9eb0e95fea4d106da42
Tobias Johansson <TobiasJohansson911@gmail.com>
2018-11-06 20:10:48 +0100

    created file calculator

---
+++ Labb1Calculator/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Labb1Calculator
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            FirstNumber = ;
+            SecondNumber = ;
+            NumberInMemory = ;
+        }
+    }
+}
