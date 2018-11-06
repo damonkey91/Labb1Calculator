691013a4441a24f983bc2a035788540649770fbb
Schwimpy <simon.winther@hotmail.com>
2018-11-06 20:38:24 +0100

    Simon added properties

--- Labb1Calculator/Calculator.cs
+++ Labb1Calculator/Calculator.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Labb1Calculator
 {
     public class Calculator
     {
+        int firstNumber { get; set; }
+        int secondNumber { get; set; }
+        List<int> NumberInMemory { get; set; }
+
         public Calculator()
         {
             FirstNumber = ;
