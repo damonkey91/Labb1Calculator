10b8d782d8464ec983da4c50ac736dba9e4d0f8c
Tobias Johansson <TobiasJohansson911@gmail.com>
2018-11-06 20:03:32 +0100

    created cases and saved

--- Labb1Calculator/Program.cs
+++ Labb1Calculator/Program.cs
@@ -6,7 +6,28 @@ namespace Labb1Calculator
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+
+            String answer = Console.ReadLine();
+            switch (answer)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
