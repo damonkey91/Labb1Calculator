75d965e00aa5c5517a5d89c3675198e61173078f
Tobias Johansson <TobiasJohansson911@gmail.com>
2018-11-06 20:35:42 +0100

    calculator constructor, to string method

--- Labb1Calculator/Calculator.cs
+++ Labb1Calculator/Calculator.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Labb1Calculator
 {
     public class Calculator
     {
         public Calculator()
         {
-            FirstNumber = ;
-            SecondNumber = ;
-            NumberInMemory = ;
+            FirstNumber = 0;
+            SecondNumber = 0;
+            NumberInMemory = new List<int>();
+        }
+
+        public override string ToString(){
+            Console.WriteLine("First number: {0} \nSecond number: {1} \nNumber in memory: {2} ", FirstNumber, SecondNumber, NumberInMemory);
         }
     }
 }
