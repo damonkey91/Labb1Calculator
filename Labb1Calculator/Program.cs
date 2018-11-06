dc516013ba3851f5ce1d8da86445931762f5375c
Tobias Johansson <TobiasJohansson911@gmail.com>
2018-11-06 20:00:05 +0100

    Created cases

---
+++ Labb1Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labb1Calculator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
